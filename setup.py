24913d43092062f5d3e5a3080b243af1329fc161
Hans Insulander <hans@codium.se>
2018-11-11 01:28:49 +0100

    Bump version, correct homepage URL, specify test suite

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='aiowintest',
-      version='0.0.1',
+      version='0.0.2.dev1',
       description='Python implementation of the Win-Test UDP protocol',
-      url='http://github.com/hin/wintest-python',
+      url='http://github.com/hin/aiowintest',
       author='Hans Insulander SM0UTY',
       author_email='hans@codium.se',
       license='BSD 2-Clause "Simplified" License',
@@ -11,4 +11,5 @@ setup(name='aiowintest',
       install_requires=[
           'asyncio',
       ],
+      test_suite='aiowintest.tests',
       zip_safe=False)
